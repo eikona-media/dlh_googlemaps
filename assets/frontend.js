f5c8561ef696feb032d27d02e0a6e17ef0a0ed8c
Ralf Baussenwein <14016098+rabauss@users.noreply.github.com>
2019-11-14 10:52:31 +0100

    refactored frontend.js

--- assets/frontend.js
+++ assets/frontend.js
@@ -1,48 +1,52 @@
-var dlh_googlemaps_cookie_name = 'dlh_googlemaps';
-var dlh_googlemaps_cookie_days = 365 * 86400;
+function DlhGoogleMaps() {
+    this.cookieName = 'dlh_googlemaps';
+    this.cookieDays = 365 * 86400;
 
-function dlh_googlemaps_get_cookie() {
-    var name = dlh_googlemaps_cookie_name + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
+    this.getCookie = function () {
+        var name = this.cookieName + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
         }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
+        return false;
+    }
+
+    this.setCookie = function () {
+        document.cookie = this.cookieName + '=ok; max-age=' + this.cookieDays + ' ; path=/';
+    }
+
+    this.loadScripts = function (map) {
+        var functionName = "gmap" + map.id + "_generate";
+        if (typeof window[functionName] == "function") {
+            var mapsScript = document.createElement('script');
+            mapsScript.type = 'text/javascript';
+            mapsScript.src = map.api_url + '&callback=' + functionName;
+            mapsScript.async = true;
+            mapsScript.defer = true;
+            document.getElementsByTagName('head')[0].appendChild(mapsScript);
         }
     }
-    return false;
-}
 
-function dlh_googlemaps_set_cookie() {
-    document.cookie = dlh_googlemaps_cookie_name + '=ok; max-age=' + dlh_googlemaps_cookie_days + ' ; path=/';
-}
+    this.askConfirmation = function (map) {
+        var functionName = "gmap" + map.id + "_allow";
+        var confirmElement = document.createElement("div");
+        confirmElement.setAttribute("id", "confirm-googlemaps");
+        confirmElement.innerHTML = map.privacy;
+        if (typeof window[functionName] == "function") {
+            confirmElement.onclick = window[functionName];
+        }
 
-function dlh_googlemaps_load_scripts(map) {
-    var function_name = "gmap" + map.id + "_generate";
-    if (typeof window[function_name] == "function") {
-        var maps_script = document.createElement('script');
-        maps_script.type = 'text/javascript';
-        maps_script.src = map.api_url + '&callback=' + function_name;
-        maps_script.async = true;
-        maps_script.defer = true;
-        document.getElementsByTagName('head')[0].appendChild(maps_script);
+        var mapElement = document.getElementById("dlh_googlemap_" + map.id);
+        mapElement.innerHTML = '';
+        mapElement.appendChild(confirmElement);
     }
 }
 
-function dlh_googlemaps_ask_confirmation(map) {
-    var function_name = "gmap" + map.id + "_allow";
-    var confirm_element = document.createElement("div");
-    confirm_element.setAttribute("id", "confirm-googlemaps");
-    confirm_element.innerHTML = map.privacy;
-    if (typeof window[function_name] == "function") {
-        confirm_element.onclick = window[function_name];
-    }
-
-    var map_element = document.getElementById("dlh_googlemap_" + map.id);
-    map_element.innerHTML = '';
-    map_element.appendChild(confirm_element);
-}
+var dlhGoogleMaps = new DlhGoogleMaps();

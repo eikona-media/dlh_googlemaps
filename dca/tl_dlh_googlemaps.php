3b4f1bdc614bd2e611d1370c334d70cce048d077
Ralf Baussenwein <r.baussenwein@eikona-media.de>
2022-03-14 11:31:10 +0100

    Fix undefined array key deleteConfirm

--- dca/tl_dlh_googlemaps.php
+++ dca/tl_dlh_googlemaps.php
@@ -78,7 +78,7 @@ $GLOBALS['TL_DCA']['tl_dlh_googlemaps'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_dlh_googlemaps']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"',
+                'attributes' => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\')) return false; Backend.getScrollOffset();"',
             ],
             'show'       => [
                 'label' => &$GLOBALS['TL_LANG']['tl_dlh_googlemaps']['show'],
--- dca/tl_dlh_googlemaps_elements.php
+++ dca/tl_dlh_googlemaps_elements.php
@@ -76,7 +76,7 @@ $GLOBALS['TL_DCA']['tl_dlh_googlemaps_elements'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_dlh_googlemaps_elements']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if (!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\')) return false; Backend.getScrollOffset();"',
+                'attributes' => 'onclick="if (!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\')) return false; Backend.getScrollOffset();"',
             ],
             'toggle' => [
                 'label'           => &$GLOBALS['TL_LANG']['tl_dlh_googlemaps_elements']['toggle'],
